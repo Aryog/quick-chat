25354a366836c4a4b4cd3290c120aaa5d9bc5091
Aryog <yogesh000aryal@gmail.com>
2025-01-04 22:15:20 +0545

    build(client): next auth

--- client/src/components/auth/LoginModal.tsx
+++ client/src/components/auth/LoginModal.tsx
@@ -1,15 +1,49 @@
-"use client";
+"use client"
 import React from "react";
-import Link from "next/link";
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+import Image from "next/image";
+import { signIn } from "next-auth/react";
+import { DialogDescription } from "@radix-ui/react-dialog";
 
-export default function Navbar({ user }: { user: CustomUser | null }) {
+const handleGoogleLogin = async () => {
+	signIn("google", {
+		redirect: true,
+		callbackUrl: "/",
+	});
+};
+
+export default function LoginModal() {
 	return (
-		<nav className="p-6 flex justify-between items-center bg-white shadow-sm">
-			<h1 className="text-xl md:text-2xl font-extrabold">QuickChat</h1>
-			<div className="flex items-center space-x-2 md:space-x-6 text-gray-700">
-				<Link href="/">Home</Link>
-				<Link href="#features">Features</Link>
-			</div>
-		</nav>
+		<Dialog>
+			<DialogTrigger asChild>
+				<Button>Getting start</Button>
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle className="text-2xl">Welcome to QuickChat</DialogTitle>
+					<DialogDescription>
+						QuickChat makes it effortless to create secure chat links and start
+						conversations in seconds.
+					</DialogDescription>
+				</DialogHeader>
+				<Button variant="outline" onClick={handleGoogleLogin}>
+					<Image
+						src="/images/google.png"
+						className=" mr-4"
+						width={25}
+						height={25}
+						alt="google"
+					/>
+					Continue with Google
+				</Button>
+			</DialogContent>
+		</Dialog>
 	);
 }
